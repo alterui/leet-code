219ce08b118276a54e0c0139c868bbe16526b415
alterui <656812771@qq.com>
2020-01-13 21:58:48 +0800

    add problem 167

---
+++ the-first-200/src/com/lr/leetcode/problem167/Application2.java
@@ -0,0 +1,50 @@
+package com.lr.leetcode.problem167;
+
+/**
+ * @author liurui
+ * @date 2020/1/13 21:29
+ * <p>
+ * 给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+ * <p>
+ * 函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+ * <p>
+ * 说明:
+ * <p>
+ * 返回的下标值（index1 和 index2）不是从零开始的。
+ * 你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+ * 示例:
+ * <p>
+ * 输入: numbers = [2, 7, 11, 15], target = 9
+ * 输出: [1,2]
+ * 解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+
+/**
+ * 双指针解法
+ * 因为数组为排好序的数组，left指针在numbers[]最左，right指针在numbers[]最右边，
+ * 然后和target进行比较。如果大于，则把right指针像左移，反之，右移动。
+ */
+public class Application2 {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            if (numbers[left] + numbers[right] > target) {
+                right--;
+            } else if (numbers[left] + numbers[right] < target) {
+                left++;
+            } else {
+                return new int[]{left + 1, right + 1};
+            }
+
+        }
+        throw new RuntimeException("");
+    }
+
+}
