7b63e1aa531985dda4fd3db46a4afc4e3dba481a
alterui <656812771@qq.com>
2020-01-16 23:50:22 +0800

    add number347

--- the-number-300/src/com/lr/leetcode/number347/Application.java
+++ the-number-300/src/com/lr/leetcode/number347/Application.java
@@ -6,44 +6,44 @@ import java.util.*;
 /**
  * @author liurui
  * @date 2020/1/13 21:10
- *
+ * <p>
  * 给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: nums = [1,1,1,2,2,3], k = 2
  * 输出: [1,2]
  * 示例 2:
- *
+ * <p>
  * 输入: nums = [1], k = 1
  * 输出: [1]
  * 说明：
- *
+ * <p>
  * 你可以假设给定的 k 总是合理的，且 1 ≤ k ≤ 数组中不相同的元素的个数。
  * 你的算法的时间复杂度必须优于 O(n log n) , n 是数组的大小。
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/top-k-frequent-elements
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Application {
     public List<Integer> topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer> hashMap = new HashMap<>();
-        for(int value:nums){
-            hashMap.put(value,hashMap.getOrDefault(value,0)+1);
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        for (int value : nums) {
+            hashMap.put(value, hashMap.getOrDefault(value, 0) + 1);
         }
 
-        PriorityQueue<Integer> queue = new PriorityQueue<>((x,y)->hashMap.get(x)-hashMap.get(y));
-        for(int key :hashMap.keySet()){
+        PriorityQueue<Integer> queue = new PriorityQueue<>((x, y) -> hashMap.get(x) - hashMap.get(y));
+        for (int key : hashMap.keySet()) {
             queue.add(key);
-            if(queue.size()>k){
+            if (queue.size() > k) {
                 queue.poll();
             }
         }
 
         List<Integer> list = new LinkedList<>();
 
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             list.add(queue.poll());
         }
 
--- the-number-300/src/com/lr/leetcode/number347/Solution.java
+++ the-number-300/src/com/lr/leetcode/number347/Solution.java
@@ -9,16 +9,16 @@ import java.util.*;
  */
 public class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int value : nums){
-            map.put(value,map.getOrDefault(value,0)+1);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int value : nums) {
+            map.put(value, map.getOrDefault(value, 0) + 1);
         }
         //构建一个list桶，桶的位置的index表示出现该数的频率
-        List[] buckets = new List[nums.length+1];
+        List[] buckets = new List[nums.length + 1];
 
-        for(int key:map.keySet()){
+        for (int key : map.keySet()) {
             int index = map.get(key);
-            if(buckets[index]==null){
+            if (buckets[index] == null) {
                 buckets[index] = new ArrayList<>();
             }
             buckets[index].add(key);
@@ -27,8 +27,8 @@ public class Solution {
         List<Integer> result = new ArrayList<>();
 
         //从后往前遍历
-        for(int i = buckets.length-1;i>=0&&result.size()<k;i--){
-            if(buckets[i]==null){
+        for (int i = buckets.length - 1; i >= 0 && result.size() < k; i--) {
+            if (buckets[i] == null) {
                 continue;
             }
             result.addAll(buckets[i]);
