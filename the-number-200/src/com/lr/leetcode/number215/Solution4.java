6c2373e7ad4fe53645272da1b37ee5a17acebc1c
alterui <656812771@qq.com>
2020-01-16 18:22:47 +0800

    add number215 Solution4

--- the-number-200/src/com/lr/leetcode/number215/Solution4.java
+++ the-number-200/src/com/lr/leetcode/number215/Solution4.java
@@ -11,46 +11,43 @@ public class Solution4 {
         int[] nums = new int[]{3, 2, 1, 5, 6, 4};
         System.out.println(new Solution4().findKthLargest(nums, 2));
     }
+
     public int findKthLargest(int[] nums, int k) {
         int heapSize = 0;
-        for(int i =0;i<nums.length;i++){
+        for (int i = 0; i < nums.length; i++) {
 
-            if (heapSize<k){
-                heapSize = heapInsert(nums,i,heapSize);
-            }else {
-
-                nums[k]= nums[i];
-                heapSize = heapInsert(nums,k,heapSize);
+            if (heapSize < k) {
+                heapSize = heapInsert(nums, i, heapSize);
+            } else {
+                nums[k] = nums[i];
+                heapSize = heapInsert(nums, k, heapSize);
 
             }
 
-
-
-            if(heapSize>k){
+            if (heapSize > k) {
                 //弹出0，然后变成小顶堆
-                swap(nums,0,--heapSize);
-                heapify(nums,0,heapSize);
+                swap(nums, 0, --heapSize);
+                heapify(nums, 0, heapSize);
 
             }
         }
         return nums[0];
 
 
-
     }
 
-    public void heapify(int[] nums,int index,int heapSize){
-        int left = 2*index +1;
-        while(left<heapSize){
-            int smallIndex = left+1<heapSize&& nums[left+1]<nums[left] ? left+1:left;
-            smallIndex = nums[smallIndex]<nums[index] ? smallIndex:index;
-            if(smallIndex==index){
+    public void heapify(int[] nums, int index, int heapSize) {
+        int left = 2 * index + 1;
+        while (left < heapSize) {
+            int smallIndex = left + 1 < heapSize && nums[left + 1] < nums[left] ? left + 1 : left;
+            smallIndex = nums[smallIndex] < nums[index] ? smallIndex : index;
+            if (smallIndex == index) {
                 break;
             }
 
-            swap(nums,index,smallIndex);
+            swap(nums, index, smallIndex);
             index = smallIndex;
-            left = 2*index+1;
+            left = 2 * index + 1;
 
         }
 
@@ -58,16 +55,16 @@ public class Solution4 {
 
 
     //建立小根堆,并返回堆的size
-    public int heapInsert(int[] nums,int index,int heapSize){
+    public int heapInsert(int[] nums, int index, int heapSize) {
         ++heapSize;
-        while(nums[index]<nums[(index-1)/2]){
-            swap(nums,index,(index-1)/2);
-            index = (index-1)/2;
+        while (nums[index] < nums[(index - 1) / 2]) {
+            swap(nums, index, (index - 1) / 2);
+            index = (index - 1) / 2;
         }
         return heapSize;
     }
 
-    public void swap(int[] nums,int i ,int j){
+    public void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
